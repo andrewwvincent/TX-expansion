2c797b35de62ab73c0f300f9e1e6abc73cea0a43
andrewwvincent <andrew.vincent@trilogy.com>
2025-03-04 18:21:52 -0500

    Closed schools in Texas

--- config.js
+++ config.js
@@ -11,60 +11,10 @@ export const config = {
     popupInfo: ["Location_Name"],
     locationLayers: [
         {
-            id: 'pending-partner',
-            name: 'Pending Partners',
-            file: 'data/pending_partner.kml',
-            color: '#FF0000',  // Red
-            shape: 'circle',  // Options: circle, square, triangle, star
-            size: 16, // Size in pixels
-            defaultChecked: true,
-            defaultLabels: false
-        },
-        {
-            id: 'secondary-partners',
-            name: 'Secondary Partners',
-            file: 'data/secondary_partner.kml',
-            color: '#0000FF',  // Blue
-            shape: 'circle',
-            size: 16,
-            defaultChecked: false,
-            defaultLabels: false
-        },
-        {
-            id: 'partners',
-            name: 'Current Partners',
-            file: 'data/partner.kml',
-            color: '#00FF00',  // Green
-            shape: 'circle',
-            size: 16,
-            defaultChecked: false,
-            defaultLabels: false
-        },
-        {
             id: 'targets',
-            name: 'Potential Targets',
+            name: 'Closed or Potentially Closing Schools',
             file: 'data/target.kml',
             color: '#FF00FF',  // Purple
-            shape: 'circle',
-            size: 16,
-            defaultChecked: false,
-            defaultLabels: false
-        },
-        {
-            id: 'redline',
-            name: 'Redline Athletics Locations',
-            file: 'data/redline_locations.kml',
-            color: '#FFA500',  // Orange
-            shape: 'circle',
-            size: 16,
-            defaultChecked: false,
-            defaultLabels: false
-        },
-        {
-            id: 'D1',
-            name: 'D1 Training Locations',
-            file: 'data/d1_locations.kml',
-            color: '#808080',  // Grey
             shape: 'circle',
             size: 16,
             defaultChecked: false,
